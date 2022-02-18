a0833e37155d5837d36a0fff1993b1ec1853f529
Natalia Morales <nataliamgal95@gmail.com>
2022-02-18 12:16:18 -0500

    Variables Let VS Var, DOM, eventos en javaScript, exportar modulos

---
+++ main.js
@@ -0,0 +1,13 @@
+function prueba(){
+    //Cualquier asignacion es directa, por lo cual obtengo un producto normal (resultado de cualquier proceso)
+    //Var y Let son mutables y Const inmutable
+    //Let NO trabaja por bloques
+    //Var SI trabaja por bloques
+    //La diferencia entre var y let esta en que let trabaja por posiciones y var por bloques
+    //El proceso con las constantes no imprimen nada, let no define el error y var si define el error por bloques
+    //Las tres en cualquier definicion de proceso, se caracterizan por determinar un error, si el proceso al inicio es indefinido el final tambien es indefinido
+    
+    var a = 5;
+    console.log(a);
+}
+prueba();
